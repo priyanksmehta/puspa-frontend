879052833dca38a8b465eda6eefd85420c2c208c
priyanksmehta <priyanksanjaymehta@gmail.com>
2021-07-05 03:29:33 +0530

    @refactor: made footer mobile responsive

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,27 +1,31 @@
 <div
-    class="grid grid-cols-2 bg-footer-background justify-items-center text-white">
-    <div class="ml-52 mr-40">
+    class="grid grid-cols-1 lg:grid-cols-2 bg-footer-background lg:justify-items-center text-white">
+    <div class="lg:ml-52 lg:mr-40 mx-10">
         <div class="mb-5 mt-10">Stay Posted</div>
         <div class="text-[#9ca1b2]">
             Subscribe to our newsletter to receive updates on new launches
         </div>
-        <div class="mt-5 border-b-[1px] border-white">
-            <div>
-                <input
-                    class="bg-transparent"
-                    type="text"
-                    placeholder="Your mail ID" />
-                <button class="float-right">SUBMIT</button>
-            </div>
+        <div class="mt-5 border-b-[1px] border-white flex">
+            <input
+                class="bg-transparent w-full"
+                type="text"
+                placeholder="Your email" />
+            <button>SUBMIT</button>
         </div>
-        <div class="mt-10">© 2021 Puspa. All Rights Reserved.</div>
+        <div class="mt-10 lg:block hidden">
+            © 2021 Puspa Group All Rights Reserved.
+        </div>
     </div>
-    <div class="mt-10 mb-10">
+    <div class="lg:mt-10 lg:mb-10 mt-10 ml-10">
         <div>Drop us a line</div>
         <br />
         <div class="text-[#9ca1b2]">
-            +91 80 23155031<br /><br />gs@puspagroup.in<br /><br />25, SSI Area,
-            5th Cross<br />5th Block, Rajajinagar <br />Bangalore - 560010
+            25, SSI Area, 5th Cross<br />5th Block, Rajajinagar <br />Bangalore
+            - 560010
+            <br />+91 80 23155031<br />gs@puspagroup.in
         </div>
+    </div>
+    <div class="lg:hidden block ml-10 mt-10">
+        © 2021 Puspa Group All Rights Reserved.
     </div>
 </div>
