ba19b077bb700bd603e6e9b33acaba2dc914d402
priyanksmehta <priyanksanjaymehta@gmail.com>
2021-07-05 02:26:11 +0530

    @refactor: made contact page mobile responsive

--- src/lib/components/Contact Us/ContactForm.svelte
+++ src/lib/components/Contact Us/ContactForm.svelte
@@ -2,41 +2,43 @@
     import { text } from "svelte/internal";
 </script>
 
-<div class="grid grid-cols-[20%,40%,20%,20%]">
+<div class="grid grid-cols-1 lg:grid-cols-[20%,40%,20%,20%]">
     <div />
     <div>
         <div class="mt-40 text-3xl">Contact</div>
         <div class="grid grid-rows-[auto,auto,1fr, auto]">
-            <div class="mt-8 flex">
-                <span class="border-[1px] mr-8 w-6/12">
+            <div class="grid grid-cols-1 lg:flex">
+                <span class="border-[1px] mr-8 mt-4 lg:mt-8 lg:w-6/12 w-full">
                     <input
                         class="p-2 w-full"
                         type="text"
                         placeholder="Full Name" />
                 </span>
-                <span class="border-[1px] w-6/12">
+                <span class="border-[1px] mt-2 lg:mt-8 lg:w-6/12 w-full">
                     <input
                         class="p-2 w-full"
                         type="text"
                         placeholder="Contact No." />
                 </span>
             </div>
-            <div class="flex mt-4">
-                <div class="border-[1px] mr-8 w-1/2">
+            <div class="grid grid-cols-1 lg:flex lg:mt-4 gap-y-2 mt-2">
+                <div class="border-[1px] lg:mr-8 lg:w-1/2">
                     <input
                         class="p-2 w-full"
                         type="text"
                         placeholder="Email Address" />
                 </div>
-                <div class="border-[1px] w-1/2">
+                <div class="border-[1px] lg:w-1/2">
                     <input
                         class="p-2 w-full"
                         type="text"
                         placeholder="Subject" />
                 </div>
             </div>
-            <div class="flex mt-4 border-[1px] h-32">
-                <textarea class="resize-none w-full" placeholder="Message" />
+            <div class="flex mt-2 lg:mt-4 border-[1px] h-32">
+                <textarea
+                    class="resize-none w-full px-2"
+                    placeholder="Message" />
             </div>
             <div class="mt-4 float-right text-right">
                 <button class="bg-cart-button text-white px-10 py-2"
@@ -45,8 +47,8 @@
         </div>
     </div>
     <div>
-        <div class="mt-56 ml-14 text-2xl">Locate Us</div>
-        <div class="mt-8 ml-14 text-gray-500">
+        <div class="mt-56 lg:ml-14 ml-4 text-2xl">Locate Us</div>
+        <div class="mt-8 lg:ml-14 ml-4 text-gray-500">
             <p class="text-gray-700 text-xl">Corporate Office:</p>
             <p class="mt-2">
                 91/13, SSI Area, 4th Cross<br />
@@ -57,7 +59,7 @@
             <p>+91 80 23155031</p>
             <p>puspa@puspagroup.in</p>
         </div>
-        <div class="mt-8 mb-10 ml-14 text-gray-500">
+        <div class="mt-8 mb-10 ml-4 lg:ml-14 text-gray-500">
             <p class="text-gray-700 text-xl">Global Specialities:</p>
             <p class="mt-2">
                 25, SSI Area, 5th Cross<br />
