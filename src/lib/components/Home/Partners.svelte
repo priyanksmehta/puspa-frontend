79a2bb17f9966490b01d2dd31e54dffbc8e01f3c
priyanksmehta <priyanksanjaymehta@gmail.com>
2021-07-05 02:10:18 +0530

    @refactor: made partners screen mobile responsive

--- src/lib/components/Home/Partners.svelte
+++ src/lib/components/Home/Partners.svelte
@@ -1,6 +1,7 @@
 <div class="grid grid-cols-1 items-center justify-items-center m-10">
-    <div class="text-4xl m-6">Our Global Partners</div>
-    <div class="flex space-x-20">
+    <div class="text-4xl m-0 lg:m-6">Our Global Partners</div>
+    <div
+        class="grid grid-cols-2 gap-x-28 gap-y-10 mt-5 lg:flex lg:space-x-20 lg:gap-x-4 items-center justify-items-center">
         <img src="/ptkarimun.png" alt="partners ptk" />
         <img src="/klkoh.png" alt="partners klk" />
         <img src="/bl.png" alt="partners bl" />
