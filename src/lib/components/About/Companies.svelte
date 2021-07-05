eb18062362d9de449a0387c8e2e3317bc18df746
priyanksmehta <priyanksanjaymehta@gmail.com>
2021-07-05 21:17:40 +0530

    @refactor: cleaned company section of about us

--- src/lib/components/About/Companies.svelte
+++ src/lib/components/About/Companies.svelte
@@ -1,72 +1,41 @@
-<div class="grid grid-cols-[50%,50%] items-center">
-    <div class="mt-10 relative inline-block overflow-hidden">
-        <img
-            class="block duration-500 ease-in-out transform hover:scale-125"
-            src="bl_companies.png"
-            alt="bl companies" />
-    </div>
-    <div class="mt-10 px-20 items-center">
-        <div class="text-4xl">Blue Lotus</div>
-        <div class="text-xl text-gray-600">
-            French Absolutes and Specialities
-        </div>
-        <div class="mt-4">
-            <p>
-                The Blue Lotus range of Naturals, Reconstitued Essential Oils,
-                and Tobacco specialities was launched in 2015 in partnership
-                with a leading French perfumery contract manufacturing company.
-            </p>
-            <p class="mt-4">
-                Its wide range of quality products has found instant acceptance
-                across various industries, and today is a market leader in many
-                segments.
-            </p>
-        </div>
-    </div>
-    <div class="px-20">
-        <div class="text-4xl">PT Karimun Kencana Aromatics</div>
-        <div class="text-xl text-gray-600">Indonesian Essential Oils</div>
-        <div class="mt-4">
-            PT Karimun Kencana Aromatics was established in 1965 in the city of
+<script>
+    const companyData = [
+        {
+            company: "Blue Lotus",
+            image: "bl_companies.png",
+            tagline: "French Absolutes and Specialities",
+            bio: `The Blue Lotus range of Naturals, Reconstitued Essential Oils,
+                    and Tobacco specialities was launched in 2015 in partnership with a 
+                    leading French perfumery contract manufacturing company.`,
+        },
+        {
+            company: "PT Karimun Kencana Aromatics",
+            image: "ptk_companies.png",
+            tagline: "Indonesian Essential Oils",
+            bio: `PT Karimun Kencana Aromatics was established in 1965 in the city of
             Medan, the chief city on the island of Sumatra, Indonesia. For the
             past 60 years, it has been one of the world's leading producers of
             Indonesian Essential Oils, led by Patchouli Oil and followed by
             Vetiver Oil, Clove Oil, Citronella, and Nutmeg Oil. The company is
             owned and led by the dynamic and enterprising Mr. Petrus Arifin,
-            whose passion for quality assurance is legendary.
-        </div>
-    </div>
-    <div class="relative inline-block overflow-hidden">
-        <img
-            class="block duration-500 ease-in-out transform hover:scale-125"
-            src="ptk_companies.png"
-            alt="ptk companies" />
-    </div>
-    <div class="relative inline-block overflow-hidden">
-        <img
-            class="block duration-500 ease-in-out transform hover:scale-125"
-            src="klk_companies.png"
-            alt="klk companies" />
-    </div>
-    <div class="px-20">
-        <div class="text-4xl">KL Koh Enterprises</div>
-        <div class="text-xl text-gray-600">Indonesian Essential Oils</div>
-        <div class="mt-4">
-            <p>
-                M/s KL Koh Enterprises, founded in 1971 by the visionary Mr. KL
+            whose passion for quality assurance is legendary.`,
+        },
+        {
+            company: "KL Koh Enterprises",
+            image: "klk_companies.png",
+            tagline: "Indonesian Essential Oils",
+            bio: `M/s KL Koh Enterprises, founded in 1971 by the visionary Mr. KL
                 Koh, is one of the pioneers of Patchouli Oil, and their premium
                 Patchouli Oil SPX is widely respected and used across India.
                 Today, Mr. KL Koh is ably and competently aided by his dynamic
                 son Mr. Edwin Koh, who has formed a large network for the export
-                of Indonesian Essential Oils across the world.
-            </p>
-        </div>
-    </div>
-    <div class="px-20">
-        <div class="text-4xl">Aromatica LF</div>
-        <div class="text-xl text-gray-600">French Premium Essential Oils</div>
-        <div class="mt-4">
-            Established in the year 2000, Aromatica LF is ably led by a leading
+                of Indonesian Essential Oils across the world.`,
+        },
+        {
+            company: "Aromatica LF",
+            image: "lf_companies.png",
+            tagline: "French Premium Essential Oils",
+            bio: `Established in the year 2000, Aromatica LF is ably led by a leading
             French Flavorist, Ms. Corrine Barrault, and assisted by the famous
             Mr. Guy Petit, who thoroughly understands the Indian Perfumery and
             Tobacco markets. The company manufactures the most widely used
@@ -75,35 +44,16 @@
             orifinal is incomparable and is favored across India, despite many
             cheaper versions floating around in the market. The company also
             manufactures the well known and widely accepted LF range of
-            products.
-        </div>
-    </div>
-    <div class="relative inline-block overflow-hidden">
-        <img
-            class="block duration-500 ease-in-out transform hover:scale-125"
-            src="lf_companies.png"
-            alt="lf companies" />
-    </div>
-    <div class="relative inline-block overflow-hidden">
-        <img
-            class="block duration-500 ease-in-out transform hover:scale-125"
-            src="fine_companies.png"
-            alt="fine companies" />
-    </div>
-    <div class="px-20">
-        <div class="text-4xl">Fine Organics</div>
-        <div class="text-xl text-gray-600">
-            Oleochemical Derivatives for Diverse Applications
-        </div>
-        <div class="mt-4">
-            <p>
-                Founded in 1970, Fine Organics introduced the Indian market to
+            products.`,
+        },
+        {
+            company: "Fine Organics",
+            image: "fine_companies.png",
+            tagline: "Oleochemical Derivatives for Diverse Applications",
+            bio: `Founded in 1970, Fine Organics introduced the Indian market to
                 high-quality additives for specialty applications with raw
                 materials from locally grown plant sources and quality levels on
-                par with international standards.
-            </p>
-            <p class="mt-4">
-                Today, Fine's range of innovative additives has expanded their
+                par with international standards. Today, Fine's range of innovative additives has expanded their
                 reach into several specialty applications in food, plastics,
                 cosmetics, textiles, paints, inks, rubbers, and manymore. A
                 technology and customer driven company, Fine Organics enjoys a
@@ -111,25 +61,59 @@
                 globally. Fine Organics owes its growth and success to its
                 expertise in oleochemistry, in-house manufacturing technology,
                 customer-centric research, and development and techno-commerical
-                mindset.
-            </p>
-        </div>
-    </div>
-    <div class="px-20">
-        <div class="text-4xl">Daiwik Pharmasphere Pvt. Ltd.</div>
-        <div class="text-xl text-gray-600">Pharma Technology</div>
-        <div class="mt-4">
-            Daiwik Pharmasphere Private Limited was established in 2010, with a
+                mindset.`,
+        },
+        {
+            company: "Daiwik Pharmasphere Pvt. Ltd.",
+            image: "daiwik_companies.jpg",
+            tagline: "Pharma Technology",
+            bio: `Daiwik Pharmasphere Private Limited was established in 2010, with a
             goal to create a WHO, and EU GMP compliant manufacturing facility
             for pharmaceuticals, gels, and ointments. The company manufactures a
             wide range of formulations primarily for contract manufacturing and
-            worldwide exports.
-        </div>
-    </div>
-    <div class="relative inline-block overflow-hidden">
-        <img
-            class="block duration-500 ease-in-out transform hover:scale-125"
-            src="daiwik_companies.jpg"
-            alt="daiwik companies" />
-    </div>
+            worldwide exports.`,
+        },
+    ];
+</script>
+
+<div class="lg:grid lg:grid-cols-[50%,50%] items-center hidden">
+    {#each companyData as company, idx}
+        {#if idx % 2 == 0}
+            <div class="relative inline-block overflow-hidden">
+                <img
+                    class="block duration-500 ease-in-out transform hover:scale-125"
+                    src={company.image}
+                    alt="alt companies" />
+            </div>
+            <div class="px-20 lg:m-0 items-center lg:block">
+                <div class="text-4xl">{company.company}</div>
+                <div class="text-xl text-gray-600">
+                    {company.tagline}
+                </div>
+                <div class="mt-4">
+                    <p>
+                        {company.bio}
+                    </p>
+                </div>
+            </div>
+        {:else}
+            <div class="px-20 lg:m-0 items-center lg:block">
+                <div class="text-4xl">{company.company}</div>
+                <div class="text-xl text-gray-600">
+                    {company.tagline}
+                </div>
+                <div class="mt-4">
+                    <p>
+                        {company.bio}
+                    </p>
+                </div>
+            </div>
+            <div class="relative inline-block overflow-hidden">
+                <img
+                    class="block duration-500 ease-in-out transform hover:scale-125"
+                    src={company.image}
+                    alt="alt companies" />
+            </div>
+        {/if}
+    {/each}
 </div>
